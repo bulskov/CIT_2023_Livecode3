467aadc7ca2c931f61f7c2cb2e540f008a5342c5
bulskov <bulskov@ruc.dk>
2023-10-13 11:09:48 +0200

    starting on linq

--- Linq/Linq.cs
+++ Linq/Linq.cs
@@ -4,7 +4,22 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello world");
+        int[] number = { 1, 2, 3, 4, 5 };
+
+        var q = number.Where(x => x >= 4);
+
+        var persons = Data.GetPersonData();
+
+        var pq = persons
+            .Where(x => x.Age < 25)
+            .Select(x => new { x.Name, x.Age });
+
+        var num = Data.GetRandomNumbers(10);
+
+        foreach (var x in num.OrderByDescending(x => x))
+        {
+            Console.WriteLine(x);
+        }
 
     }
 }
